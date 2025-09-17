7fef763ee4e4994d37f528b5efe763e06bc512f7
Pedro Henrique Martins <pepeuhenrique357@gmail.com>
2025-09-17 17:37:31 +0000

    87

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 
-use Application\Hello;
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -61,6 +61,85 @@ use Application\Hello;
 </div>
 </nav>
 
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="public/img/hamburger-h500.png" class="d-block" alt="...">
+    </div>
+         <div class="carousel-item">
+        <img src="public/img/hotdog-h500.png" class="d-block" alt="...">
+    </div>
+     <div class="carousel-item active">
+        <img src="public/img/potato-h500.png" class="d-block" alt="...">
+    </div>
+     <div class="carousel-item active">
+        <img src="public/img/snacks-h500.png" class="d-block" alt="...">
+    </div>
+    </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+                     <span class="carousel-control-prev-icon" aria hidden="true"></span>
+                     <span class="visually-hidden".>Previus</span>
+        </button>
+     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+        data-bs-slide="next">
+        <span class="carousel-co trol-next-icom" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<main class="container - 3">
+    <div class="row">
+        <div class="col">
+            <img class="card-img-top" src="public/img/hamburger-cartoon-w358.png" alt="card image cap">
+            <div class="card-body">
+                <p class="card-text">some quick example text to build on the card title and make up the bulk of the card´s contente.</p>
+            </div>
+        </div> 
+    </div>
+        <div class="col">
+            <div class="card text-white bg-warning ">
+                <img class="card-img-top" src="public/img/hotdog-cartoon-w358.png" alt="card image cap">
+                <div class="card-body">
+                    <p class="card-text">some quick example text to build on the card title and make up the bulk of the card´s contente.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-white bg-warning ">
+                <img class="card-img-top" src="public/img/potato-cartoon-w358.png" alt="card image cap">
+                <div class="card-body">
+                    <p class="card-text">some quick example text to build on the card title and make up the bulk of the card´s contente.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-white bg-warning ">
+                <img class="card-img-top" src="public/img/snacks-cartoon-w358.png" alt="card image cap">
+                <div class="card-body">
+                    <p class="card-text">some quick example text to build on the card title and make up the bulk of the card´s contente.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<footer class="text-warning bg-danger">
+    <div class="container">
+        <div class="row">
+            <div class="col" style="text-align: center;">
+                <img class="mb-2" src="public/img/ham-ico-64.png" alt="" width="64" height="64">
+                <span class="display-5">Larika</span>
+                <small class="d-bloc mb-3">&copy;2021</small>
+            </div>
+        </div>
+    </div>
+</footer>
+        
+        
+
+        
 
 
 <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" /></script>
@@ -68,3 +147,4 @@ use Application\Hello;
 
 </body>
 </html>
+
